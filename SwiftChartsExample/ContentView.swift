8f778aca5a2e611901d60ace64e7d67aa7796cce
Keita Yamamoto <yamamoto@altonotes.co.jp>
2023-11-13 18:07:37 +0900

    Update

--- SwiftChartsExample/ContentView.swift
+++ SwiftChartsExample/ContentView.swift
@@ -1,21 +1,85 @@
-//
-//  ContentView.swift
-//  SwiftChartsExample
-//
-//  Created by Keita Yamamoto on 2023/11/13.
-//
-
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    let barColors: [Color] = [
+        .red,
+        .cyan,
+        .blue
+    ]
+
+    let barChartData = [
+        [90, 65, 80],
+        [30, 50, 55],
+        [40, 80, 70]
+    ]
+
+    let lineChartData: [[Double]] = [
+        [4, -1, -10],
+        [8, 9, 7],
+        [7, 10, 18]
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart {
+            // Bar Chart
+            ForEach(Array(barChartData.enumerated()), id: \.element) { dataIndex, data in
+                ForEach(Array(data.enumerated()), id: \.element) { timeIndex, number in
+                    BarMark(
+                        x: .value("Name", "\(timeIndex)期"),
+                        y: .value("Value", number)
+                    )
+                    .foregroundStyle(by: .value("Category", "Bar\(dataIndex)"))
+//                    .foregroundStyle(barColors[dataIndex])
+                    .position(by: .value("Category", "データ\(dataIndex)"))
+                }
+            }
+            
+            // Line Chart
+            ForEach(Array(lineChartData.enumerated()), id: \.element) { dataIndex, data in
+                ForEach(Array(data.enumerated()), id: \.element) { timeIndex, number in
+                    LineMark(
+                        x: .value("Name", "\(timeIndex)期"),
+                        y: .value("Percentage\(dataIndex)", number)
+                    )
+                    .foregroundStyle(by: .value("Category", "Line\(dataIndex)"))
+                    .symbol(by: .value("Category", "Category\(dataIndex)"))
+                }
+            }
         }
-        .padding()
+//        .chartForegroundStyleScale([
+//            "Category0": .yellow,
+//            "Category1": .purple,
+//            "Category2": .teal
+//        ])
+        .chartYAxis {
+            AxisMarks(position: .leading, values: Array(stride(from: -24, through: 24, by: 30))){
+                axis in
+                AxisTick()
+                AxisGridLine()
+                AxisValueLabel("\(1014 + (axis.index * 1))", centered: false)
+            }
+            AxisMarks(position: .trailing, values: Array(stride(from: 0, through: 100, by: 30))){
+                axis in
+                AxisTick()
+                AxisGridLine()
+                AxisValueLabel("\(axis.index * 4)", centered: false)
+            }
+        }
+//        .chartLegend(.hidden)
+        .frame(height: 300)
+        .chartOverlay { proxy in
+            GeometryReader { geometry in
+                ZStack(alignment: .top) {
+                    Rectangle().fill(.clear).contentShape(Rectangle())
+                        .onTapGesture { location in
+                            let xPosition = location.x - geometry[proxy.plotAreaFrame].origin.x
+                            let xLabel: String? = proxy.value(atX: xPosition)
+                            print(xLabel ?? "nil")
+                        }
+                }
+            }
+        }
     }
 }
 
---
+++ SwiftChartsExample/ExampleChart.swift
@@ -0,0 +1,115 @@
+import Charts
+import SwiftUI
+import WeatherKit
+
+struct InteractiveLollipopChartMinimal: View {
+
+    var body: some View {
+        Chart {
+            ForEach(hours, id: \.date) {
+                LineMark(
+                    x: .value("Date", $0.date, unit: .hour),
+                    y: .value("Wind Speed", $0.windSpeed)
+                )
+                .foregroundStyle(by: .value("Value", "Wind"))
+
+                LineMark(
+                    x: .value("Date", $0.date, unit: .hour),
+                    y: .value("Pressure", ($0.pressure - 1014) * 4)
+                )
+                .foregroundStyle(by: .value("Value", "Pressure"))
+            }
+            .lineStyle(StrokeStyle(lineWidth: 4.0))
+            .interpolationMethod(.catmullRom)
+        }
+        .chartForegroundStyleScale([
+            "Pressure": .purple,
+            "Wind": .teal
+        ])
+        .chartXAxis {
+            AxisMarks(position: .bottom, values: .stride(by: .hour, count: 2)) {
+                _ in
+                AxisTick()
+                AxisGridLine()
+                AxisValueLabel(format: .dateTime.hour(), centered: true)
+            }
+        }
+        .chartYAxis {
+            AxisMarks(position: .leading, values: Array(stride(from: 0, through: 24, by: 4))){
+                axis in
+                AxisTick()
+                AxisGridLine()
+                AxisValueLabel("\(1014 + (axis.index * 1))", centered: false)
+            }
+            AxisMarks(position: .trailing, values: Array(stride(from: 0, through: 24, by: 4))){
+                axis in
+                AxisTick()
+                AxisGridLine()
+                AxisValueLabel("\(axis.index * 4)", centered: false)
+            }
+        }
+    }
+}
+
+
+#Preview {
+    InteractiveLollipopChartMinimal()
+}
+
+struct HourWeatherStruct {
+    var date: Date
+    var pressure: Double
+    var temperature: Double
+    var windSpeed: Double
+}
+
+let hours: [HourWeatherStruct] = [
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600),
+                      pressure: 1015.0,
+                      temperature: 18.2,
+                      windSpeed: 6.1),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 2),
+                      pressure: 1015.3,
+                      temperature: 18.2,
+                      windSpeed: 8.1),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 3),
+                      pressure: 1015.9,
+                      temperature: 18.2,
+                      windSpeed: 9.4),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 4),
+                      pressure: 1016.3,
+                      temperature: 18.2,
+                      windSpeed: 5.2),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 5),
+                      pressure: 1016.3,
+                      temperature: 18.2,
+                      windSpeed: 12.1),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 6),
+                      pressure: 1016.3,
+                      temperature: 18.2,
+                      windSpeed: 11.1),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 7),
+                      pressure: 1017.3,
+                      temperature: 18.2,
+                      windSpeed: 10.1),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 8),
+                      pressure: 1018.3,
+                      temperature: 18.2,
+                      windSpeed: 11.1),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 9),
+                      pressure: 1018.3,
+                      temperature: 18.2,
+                      windSpeed: 9.1),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 10),
+                      pressure: 1018.3,
+                      temperature: 18.2,
+                      windSpeed: 8.1),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 11),
+                      pressure: 1017.3,
+                      temperature: 18.2,
+                      windSpeed: 19.9),
+    HourWeatherStruct(date: Date(timeIntervalSinceNow: 3600 * 12),
+                      pressure: 1018.3,
+                      temperature: 18.2,
+                      windSpeed: 7.1),
+]
