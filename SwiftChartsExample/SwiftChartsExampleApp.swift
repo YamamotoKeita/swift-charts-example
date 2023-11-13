01e7e1ade3b0fd075d118b7848faf176c1de28b6
Keita Yamamoto <yamamoto@altonotes.co.jp>
2023-11-13 16:30:43 +0900

    First commit

---
+++ SwiftChartsExample/SwiftChartsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsExampleApp.swift
+//  SwiftChartsExample
+//
+//  Created by Keita Yamamoto on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
