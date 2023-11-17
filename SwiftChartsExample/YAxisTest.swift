60fd223e4659bf2f1e49dea39bdff03f926ce89d
Keita Yamamoto <yamamoto@altonotes.co.jp>
2023-11-17 14:59:31 +0900

    Add yaxis calculation

--- SwiftChartsExample/YAxisTest.swift
+++ SwiftChartsExample/YAxisTest.swift
@@ -2,111 +2,113 @@ import SwiftUI
 import Charts
 
 struct YAxisTest: View {
+    @State private var sliderValue: Double = 1
 
-    let barData = [190000, 65000, 80000, 65000]
-    let lineData = [-10, 8, 3, 5]
+    let baseData: [Double] = [-10, 20]
+
+    var lineData: [Double] {
+        baseData.map {
+            $0 * sliderValue + sliderValue
+        }
+    }
 
     var body: some View {
-        Chart {
-            // Bar Chart
-            ForEach(Array(barData.enumerated()), id: \.element) { timeIndex, number in
-                BarMark(
-                    x: .value("Name", "\(timeIndex)期"),
-                    y: .value("Bar", number),
-                    width: 10
-                )
-                .foregroundStyle(Color.orange)
+        VStack(spacing: 50) {
+            Chart {
+                ForEach(Array(lineData.enumerated()), id: \.element) { timeIndex, number in
+                    LineMark(
+                        x: .value("Name", "\(timeIndex)期"),
+                        y: .value("Line", number)
+                    )
+                }
             }
-            ForEach(Array(lineData.enumerated()), id: \.element) { timeIndex, number in
-                LineMark(
-                    x: .value("Name", "\(timeIndex)期"),
-                    y: .value("Line", number)
-                )
+            .chartYAxis {
+                AxisMarks(position: .leading, values: yAxisForLine())
             }
+            .frame(height: 300)
+
+            Slider(value: $sliderValue, in: 1...100)
+
+            Text(lineData.map { String($0) }.joined(separator: ", "))
         }
-        .chartYAxis {
-            AxisMarks(position: .leading, values: yAxisForBar())
-            AxisMarks(position: .trailing, values: yAxisForBar()) { axis in
-                let value = yAxisForLine()[axis.index]
-                AxisValueLabel("\(Int(value))", centered: false)
-            }
-        }
-        .chartYAxisLabel("(兆円)", position: .topLeading)
-        .chartYAxisLabel("(%)", position: .topTrailing)
-        .frame(height: 300)
+        .padding(.horizontal, 20)
     }
 
     func yAxisForLine() -> [Double] {
-        guard let iMax = lineData.max(), let iMin = lineData.min() else {
+        guard let max = lineData.max(), let min = lineData.min() else {
             return []
         }
-        let max = Double(iMax)
-        let min = Double(iMin)
-
-        let range = if max == min {
-            10.0
-        } else {
-            max - min
-        }
-
-        let marginedMin = min - (range * 0.1)
-        let marginedMax = max + (range * 0.1)
-
-        let minInterval = (marginedMax - marginedMin) / 5
-        let size = String(Int(minInterval)).count
-        let suffix = pow(10.0, Double(size) - 2)
-        let base = ceil(minInterval / suffix)
-        let units: [Double] = [10, 12, 15, 20, 25, 30, 40, 50, 60, 80, 100]
-        let unit = units.first {
-            $0 >= base
-        } ?? units.last!
-
-        let interval = round(unit * suffix)
-        let remainder = marginedMin.truncatingRemainder(dividingBy: interval)
-        let graphMin = (marginedMin - remainder) - interval
-        // TODO 多分マイナスの場合とプラスの場合で場合わけ
-        print("\(marginedMin) \(remainder) \(interval)")
-
-        return (0...5).map {
-            graphMin + (Double($0) * interval)
-        }
-    }
-
-//    func canContain(interval: Double, min: Double, max: Double) -> Bool {
-//        
-//    }
-
-    func yAxisForBar() -> [Double] {
-        guard let max = barData.max() else {
-            return []
-        }
-
-        return yAxis(min: 0, max: Double(max) * 1.1)
-    }
-
-    func yAxis(min: Double, max: Double) -> [Double] {
-        let interval = calcInterval(min: min, max: max)
-        return (0...5).map {
-            min + (Double($0) * interval)
-        }
-    }
-
-    func calcInterval(min: Double, max: Double) -> Double {
-        let units: [Double] = [10, 12, 15, 20, 25, 30, 40, 50, 60, 80, 100]
 
         let range = max - min
-        let minInterval = range / 5
-        let size = String(Int(minInterval)).count
-        // TODO sizeが3以下のときの考慮
-        let suffix = pow(10.0, Double(size) - 2)
-        let base = ceil(minInterval / suffix)
-        let unit = units.first { $0 >= base } ?? units.last!
-        let interval = round(unit * suffix)
-
-        return interval
+        let marginedMax = max + (range * 0.1)
+        return YAxisUtil.calcYAxis(
+            min: min,
+            max: marginedMax,
+            lineCount: 6,
+            minSpan: 10
+        )
     }
 }
 
 #Preview {
     YAxisTest()
+}
+
+enum YAxisUtil {
+    static private let units: [Double] = [10, 12, 15, 20, 25, 30, 40, 50, 60, 80]
+
+    static func calcYAxis(min: Double, max: Double, lineCount: Int, minSpan: Double) -> [Double] {
+        var safeMin = min
+        var safeMax = max
+        if min == max {
+            safeMin = min - minSpan / 2
+            safeMax = max + minSpan / 2
+        }
+
+        let lowerLimit = (safeMax - safeMin) / (Double(lineCount) - 1)
+        let length = String(Int(lowerLimit)).count
+        let suffix = pow(10.0, Double(length) - 2)
+        let prefix = ceil(lowerLimit / suffix)
+
+        var unitScale: Double = 1
+        var unitIndex = 0
+        let proceedUnitIndex = {
+            if unitIndex < units.count - 1 {
+                unitIndex += 1
+            } else {
+                unitScale *= 10
+                unitIndex = 0
+            }
+        }
+
+        var interval: Double = 0
+        var graphMax: Double = 0
+        while true {
+            let unit = units[unitIndex] * unitScale
+            if unit < prefix {
+                proceedUnitIndex()
+                continue
+            }
+
+            interval = unit * suffix
+            let remainder = safeMax.truncatingRemainder(dividingBy: interval)
+
+            graphMax = safeMax - remainder
+            if 0 < remainder {
+                graphMax += interval
+            }
+
+            let graphMin = graphMax - interval * (Double(lineCount) - 1)
+            if safeMin > graphMin {
+                break
+            }
+            proceedUnitIndex()
+        }
+
+        return (0..<lineCount)
+            .reversed()
+            .map {
+                graphMax - (Double($0) * interval)
+            }
+    }
 }
